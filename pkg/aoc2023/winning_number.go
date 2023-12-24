0db92576d8bea861dc6a8a15958d903b1fdc9d29
Tantatorn <timefear007@gmail.com>
2023-12-24 08:27:48 +0700

    ♻️ refactor scratchcard accumalator using hof

--- pkg/aoc2023/winning_number.go
+++ pkg/aoc2023/winning_number.go
@@ -1,7 +1,6 @@
 package aoc2023
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 )
@@ -11,13 +10,21 @@ func Solve1(input string) int {
 
 	count := 0
 	for _, line := range lines {
-		count += countLine(line)
+		count += countLine(line, func(i int) int {
+			if i == 0 {
+				i++
+			} else {
+				i *= 2
+			}
+
+			return i
+		})
 	}
 
 	return count
 }
 
-func countLine(input string) int {
+func countLine(input string, accumulator func(int) int) int {
 	input = strings.Split(input, ":")[1]
 
 	inputStrArr := strings.Split(input, "|")
@@ -50,11 +57,7 @@ func countLine(input string) int {
 	for _, myNum := range myNums {
 		for _, winningNum := range winningNums {
 			if myNum == winningNum {
-				if count == 0 {
-					count++
-				} else {
-					count *= 2
-				}
+				count = accumulator(count)
 			}
 		}
 	}
@@ -69,7 +72,10 @@ func Solve2(input string) int {
 	counts := make([]int, len(lines))
 
 	for i, line := range lines {
-		matched := countLine2(line)
+		matched := countLine(line, func(i int) int {
+			i++
+			return i
+		})
 
 		matchedArr[i] = matched
 		counts[i] = 1
@@ -89,56 +95,9 @@ func Solve2(input string) int {
 
 	ret := 0
 
-	fmt.Println(len(counts))
-
 	for _, c := range counts {
 		ret += c
 	}
 
 	return ret
-}
-
-func countLine2(input string) int {
-	input = strings.Split(input, ":")[1]
-
-	inputStrArr := strings.Split(input, "|")
-
-	winningStrings := strings.Split(inputStrArr[0], " ")
-
-	var winningNums []int
-	for _, numStr := range winningStrings {
-		if numStr == "" || numStr == " " {
-			continue
-		}
-
-		num, err := strconv.Atoi(numStr)
-		if err != nil {
-			panic(err)
-		}
-
-		winningNums = append(winningNums, num)
-	}
-
-	myStrings := strings.Split(inputStrArr[1], " ")
-
-	myNums := make([]int, len(myStrings))
-	for _, numStr := range myStrings {
-		if numStr == "" || numStr == " " {
-			continue
-		}
-
-		num, _ := strconv.Atoi(numStr)
-		myNums = append(myNums, num)
-	}
-
-	count := 0
-	for _, myNum := range myNums {
-		for _, winningNum := range winningNums {
-			if myNum == winningNum {
-				count++
-			}
-		}
-	}
-
-	return count
 }
--- pkg/aoc2023/winning_number_test.go
+++ pkg/aoc2023/winning_number_test.go
@@ -4,32 +4,6 @@ import (
 	"testing"
 )
 
-func Test_countLine(t *testing.T) {
-	type args struct {
-		input string
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{
-			name: "success",
-			args: args{
-				input: "1: 1 2 3 4 5 6 7 8 9 10 | 1 2 3 4 5 6 7 8 9 10",
-			},
-			want: 10,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := countLine(tt.args.input); got != tt.want {
-				t.Errorf("countLine() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestSolve1(t *testing.T) {
 	type args struct {
 		input string
