f4a1239c5103ace386316d728493f57a85b699c0
Tantatorn <timefear007@gmail.com>
2024-12-06 22:38:34 +0700

    ✨ refactor IsLevelSafe and add IsLevelSafe2

--- pkg/aoc2024/red_nosed.go
+++ pkg/aoc2024/red_nosed.go
@@ -12,11 +12,7 @@ func IsLevelSafe(level []int) bool {
 			}
 		}
 
-		if diff > 3 {
-			return false
-		} else if diff < -3 {
-			return false
-		} else if diff == 0 {
+		if diff > 3 || diff < -3 || diff == 0 {
 			return false
 		}
 
@@ -27,27 +23,20 @@ func IsLevelSafe(level []int) bool {
 }
 
 func IsLevelSafe2(level []int) bool {
-	var prev *int
-
-	for i := 0; i < len(level)-1; i++ {
-		diff := level[i+1] - level[i]
-
-		if prev != nil {
-			if (*prev > 0 && diff < 0) || (*prev < 0 && diff > 0) {
-				return false
-			}
-		}
-
-		if diff > 3 {
-			return false
-		} else if diff < -3 {
-			return false
-		} else if diff == 0 {
-			return false
-		}
-
-		prev = &diff
+	if IsLevelSafe(level) {
+		return true
 	}
 
-	return true
+	return isLevelSafe2(0, level)
+}
+
+func isLevelSafe2(i int, level []int) bool {
+	if i < len(level) {
+		lev := append([]int{}, level[:i]...)
+		lev = append(lev, level[i+1:]...)
+
+		return IsLevelSafe(lev) || isLevelSafe2(i+1, level)
+	}
+
+	return false
 }
